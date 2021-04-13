b520d541b008d13a704c01e9036e8652ff70cdcb
amit <amitmaurya788@gmail.com>
2021-04-14 04:43:52 +0530

    decouple

--- dental/settings.py
+++ dental/settings.py
@@ -1,7 +1,7 @@
 import os
 import django_heroku
 import dj_database_url
-from decoupal import config
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
